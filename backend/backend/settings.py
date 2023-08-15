5138734daa2c3c6dcf19ac864e724a1c64887b84
Полина Киселева <polly-kiselyova@yandex.ru>
2023-08-15 16:44:55 +0400

    flake4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': ('django.contrib.auth.password_validation.UserAttribute'
+                 'SimilarityValidator'),
     },
     {
         'NAME':
